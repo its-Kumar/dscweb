9d31f4a4368a30973654ac7d0e15165cd62e9880
its-Kumar <kumarshanu1009@gmail.com>
2020-10-18 12:36:47 +0530

    Enhanced homepage fix: navbar link active class on click after refresh fix: padding between icons and links in navbar as well as in menues fix: padding between logo and title fix: wrong written css properties added: scroll to various section option on home page
     Changes committed:
    	deleted:    static/js/about.js
    	deleted:    static/js/contact.js
    	modified:   static/js/custom.js
    	deleted:    static/js/destinations.js
    	deleted:    static/js/news.js

--- static/js/custom.js
+++ static/js/custom.js
@@ -16,259 +16,238 @@
 
 ******************************/
 
-$(document).ready(function()
-{
-	"use strict";
+$(document).ready(function () {
+    "use strict";
 
-	/* 
+    /*
 
-	1. Vars and Inits
+    1. Vars and Inits
 
-	*/
+    */
 
-	var header = $('.header');
-	var headerSocial = $('.header_social');
-	var menu = $('.menu');
-	var menuActive = false;
-	var burger = $('.hamburger');
+    var header = $('.header');
+    var headerSocial = $('.header_social');
+    var menu = $('.menu');
+    var menuActive = false;
+    var burger = $('.hamburger');
 
-	setHeader();
+    setHeader();
 
-	$(window).on('resize', function()
-	{
-		setHeader();
+    $(window).on('resize', function () {
+        setHeader();
 
-		setTimeout(function()
-		{
-			$(window).trigger('resize.px.parallax');
-		}, 375);
-	});
+        setTimeout(function () {
+            $(window).trigger('resize.px.parallax');
+        }, 375);
+    });
 
-	$(document).on('scroll', function()
-	{
-		setHeader();
-	});
+    $(document).on('scroll', function () {
+        setHeader();
+    });
 
-	initMenu();
-	initHomeSlider();
-	initIsotope();
-	initTestimonialsSlider();
-	initScrolling();
-	initInput();
+    initMenu();
+    initHomeSlider();
+    initIsotope();
+    initTestimonialsSlider();
+    initScrolling();
+    initInput();
 
-	/* 
+    /*
 
-	2. Set Header
+    2. Set Header
 
-	*/
+    */
 
-	function setHeader()
-	{
-		if($(window).scrollTop() > 127)
-		{
-			header.addClass('scrolled');
-			headerSocial.addClass('scrolled');
-		}
-		else
-		{
-			header.removeClass('scrolled');
-			headerSocial.removeClass('scrolled');
-		}
-	}
+    function setHeader() {
+        if ($(window).scrollTop() > 127) {
+            header.addClass('scrolled');
+            headerSocial.addClass('scrolled');
+        }
+        else {
+            header.removeClass('scrolled');
+            headerSocial.removeClass('scrolled');
+        }
+    }
 
-	/* 
+    /*
 
-	3. Set Menu
+    3. Set Menu
 
-	*/
+    */
 
-	function initMenu()
-	{
-		if($('.menu').length)
-		{
-			var menu = $('.menu');
-			if($('.hamburger').length)
-			{
-				burger.on('click', function()
-				{
-					if(menuActive)
-					{
-						closeMenu();
-					}
-					else
-					{
-						openMenu();
-					}
-				});
-			}
-		}
-		if($('.menu_close').length)
-		{
-			var close = $('.menu_close');
-			close.on('click', function()
-			{
-				if(menuActive)
-				{
-					closeMenu();
-				}
-			});
-		}
-	}
+    function initMenu() {
+        if ($('.menu').length) {
+            var menu = $('.menu');
+            if ($('.hamburger').length) {
+                burger.on('click', function () {
+                    if (menuActive) {
+                        closeMenu();
+                    }
+                    else {
+                        openMenu();
+                    }
+                });
+            }
+        }
+        if ($('.menu_close').length) {
+            var close = $('.menu_close');
+            close.on('click', function () {
+                if (menuActive) {
+                    closeMenu();
+                }
+            });
+        }
+    }
 
-	function openMenu()
-	{
-		menu.addClass('active');
-		menuActive = true;
-	}
+    function openMenu() {
+        menu.addClass('active');
+        menuActive = true;
+    }
 
-	function closeMenu()
-	{
-		menu.removeClass('active');
-		menuActive = false;
-	}
+    function closeMenu() {
+        menu.removeClass('active');
+        menuActive = false;
+    }
 
-	/* 
+    /*
 
-	4. Init Home Slider
+    4. Init Home Slider
 
-	*/
+    */
 
-	function initHomeSlider()
-	{
-		if($('.home_slider').length)
-		{
-			var homeSlider = $('.home_slider');
-			homeSlider.owlCarousel(
-			{
-				items:1,
-				autoplay:false,
-				loop:true,
-				nav:false,
-				dots:false,
-				smartSpeed:1200
-			});
-		}
-	}
+    function initHomeSlider() {
+        if ($('.home_slider').length) {
+            var homeSlider = $('.home_slider');
+            homeSlider.owlCarousel(
+                {
+                    items: 1,
+                    autoplay: true,
+                    loop: true,
+                    nav: false,
+                    dots: false,
+                    smartSpeed: 1200
+                });
+        }
+    }
 
-	/* 
+    /*
 
-	5. Init Scrolling
+    5. Init Scrolling
 
-	*/
+    */
 
-	function initScrolling()
-	{
-		if($('.home_page_nav ul li a').length)
-		{
-			var links = $('.home_page_nav ul li a');
-	    	links.each(function()
-	    	{
-	    		var ele = $(this);
-	    		var target = ele.data('scroll-to');
-	    		ele.on('click', function(e)
-	    		{
-	    			e.preventDefault();
-	    			$(window).scrollTo(target, 1500, {offset: -90, easing: 'easeInOutQuart'});
-	    		});
-	    	});
-		}	
-	}
+    function initScrolling() {
+        if ($('.home_page_nav ul li a').length) {
+            var links = $('.home_page_nav ul li a');
+            links.each(function () {
+                var ele = $(this);
+                var target = ele.data('scroll-to');
+                ele.on('click', function (e) {
+                    e.preventDefault();
+                    $(window).scrollTo(target, 1500, { offset: -90, easing: 'easeInOutQuart' });
+                });
+            });
+        }
+    }
 
-	/* 
+    /*
 
-	6. Init Isotope
+    6. Init Isotope
 
-	*/
+    */
 
-	function initIsotope()
-	{
-		if($('.item_grid').length)
-		{
-			var grid = $('.item_grid').isotope({
-				itemSelector: '.item',
-	            getSortData:
-	            {
-	            	price: function(itemElement)
-	            	{
-	            		var priceEle = $(itemElement).find('.destination_price').text().replace( 'From $', '' );
-	            		return parseFloat(priceEle);
-	            	},
-	            	name: '.destination_title a'
-	            },
-	            animationOptions:
-	            {
-	                duration: 750,
-	                easing: 'linear',
-	                queue: false
-	            }
-	        });
-		}
-	}
+    function initIsotope() {
+        if ($('.item_grid').length) {
+            var grid = $('.item_grid').isotope({
+                itemSelector: '.item',
+                getSortData:
+                {
+                    price: function (itemElement) {
+                        var priceEle = $(itemElement).find('.destination_price').text().replace('From $', '');
+                        return parseFloat(priceEle);
+                    },
+                    name: '.destination_title a'
+                },
+                animationOptions:
+                {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+        }
+    }
 
-	/* 
+    /*
 
-	7. Init Testimonials Slider
+    7. Init Testimonials Slider
 
-	*/
+    */
 
-	function initTestimonialsSlider()
-	{
-		if($('.testimonials_slider').length)
-		{
-			var testSlider = $('.testimonials_slider');
-			testSlider.owlCarousel(
-			{
-				animateOut: 'fadeOut',
-    			animateIn: 'flipInX',
-				items:1,
-				autoplay:true,
-				loop:true,
-				smartSpeed:1200,
-				dots:false,
-				nav:false
-			});
-		}
-	}
+    function initTestimonialsSlider() {
+        if ($('.testimonials_slider').length) {
+            var testSlider = $('.testimonials_slider');
+            testSlider.owlCarousel(
+                {
+                    animateOut: 'fadeOut',
+                    animateIn: 'flipInX',
+                    items: 1,
+                    autoplay: true,
+                    loop: true,
+                    smartSpeed: 1200,
+                    dots: false,
+                    nav: false
+                });
+        }
+    }
 
-	/* 
+    /*
 
-	8. Init Input
+    8. Init Input
 
-	*/
+    */
 
-	function initInput()
-	{
-		if($('.newsletter_input').length)
-		{
-			var inpt = $('.newsletter_input');
-			inpt.each(function()
-			{
-				var ele = $(this);
-				var border = ele.next();
+    function initInput() {
+        if ($('.newsletter_input').length) {
+            var inpt = $('.newsletter_input');
+            inpt.each(function () {
+                var ele = $(this);
+                var border = ele.next();
 
-				ele.focus(function()
-				{
-					border.css({'visibility': "visible", 'opacity': "1"});
-				});
-				ele.blur(function()
-				{
-					border.css({'visibility': "hidden", 'opacity': "0"});
-				});
+                ele.focus(function () {
+                    border.css({ 'visibility': "visible", 'opacity': "1" });
+                });
+                ele.blur(function () {
+                    border.css({ 'visibility': "hidden", 'opacity': "0" });
+                });
 
-				ele.on("mouseenter", function()
-				{
-					border.css({'visibility': "visible", 'opacity': "1"});
-				});
+                ele.on("mouseenter", function () {
+                    border.css({ 'visibility': "visible", 'opacity': "1" });
+                });
 
-				ele.on("mouseleave", function()
-				{
-					if(!ele.is(":focus"))
-					{
-						border.css({'visibility': "hidden", 'opacity': "0"});
-					}
-				});
-				
-			});
-		}
-	}
+                ele.on("mouseleave", function () {
+                    if (!ele.is(":focus")) {
+                        border.css({ 'visibility': "hidden", 'opacity': "0" });
+                    }
+                });
+
+            });
+        }
+    }
 });
+
+$(document).on('click', 'ul li', function () {
+    $(this).addClass('active').siblings().removeClass('active');
+    localStorage.setItem("clicked", $(this).index());
+})
+
+$('ul').on('click', 'li', function () {
+    $('ul li.active').removeClass('active');
+    $(this).addClass('active');
+})
+window.onload = function () {
+    var clicked = localStorage.getItem('clicked');
+    console.log(clicked);
+    $('ul li.active').removeClass('active');
+    $('ul li').eq(clicked).addClass('active');
+}
