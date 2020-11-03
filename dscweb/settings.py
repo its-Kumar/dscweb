2f6e8103bbe36c921ce9c0a08d86c5a12aa31fbe
its_Kumar <49340432+its-Kumar@users.noreply.github.com>
2020-11-03 21:30:59 +0530

    Update Email user

--- dscweb/settings.py
+++ dscweb/settings.py
@@ -159,8 +159,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'events.dscrecbijnor@gmail.com'
-EMAIL_HOST_PASSWORD = 'gycitowrmbeqssmc'
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER'] 
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 # EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 
 # CkEditor
