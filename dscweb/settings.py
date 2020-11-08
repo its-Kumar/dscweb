7ca96257dda96de612f265856407a125587fe4ca
its-Kumar <kumarshanu1009@gmail.com>
2020-11-08 12:35:29 +0530

    switch from `sqlite3` to `postgresql` db

--- dscweb/settings.py
+++ dscweb/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'dscweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dscweb',
+        'USER': 'dscwebuser',
+        'PASSWORD': os.environ['PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -165,7 +169,7 @@ CKEDITOR_UPLOAD_PATH = 'uploads/'
 
 # AWS S3 File storage
 
-# Secrets
+"""# Secrets
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = 'dscweb-bucket'
@@ -175,3 +179,4 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+"""
