693193c0ee48b73785a9f46bbccec52c810ce21b
kumar <kumarshanu1009@gmail.com>
2021-06-14 13:34:51 +0530

    fix settings path in `wsgi` and `asgi` files

--- dscweb/asgi.py
+++ dscweb/asgi.py
@@ -11,6 +11,12 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dscweb.settings")
+if "ENVIRONMENT_NAME" in os.environ:
+    environment_name = os.environ['ENVIRONMENT_NAME']
+    environment_settings_file = "dscweb.settings."+environment_name
+else:
+    environment_settings_file = "dscweb.settings.base"
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", environment_settings_file)
 
 application = get_asgi_application()
